77d8b86f9ab0581280e72921ad84d5fb47786d5c
ramriddlez <ramankangz@outlook.com>
2022-02-01 20:16:04 -0500

    changes in the js file

--- assets/script.js
+++ assets/script.js
@@ -1,98 +1,37 @@
 //use moment.js to format the following variables:
 let today = moment().format("dddd, MMMM Do YYYY,");
+let now = moment();
 
 //this will be the time block using JQuery
 $('#currentDay').text(today);
 
-//set datta attributre
-
-
-// For loop to multiply rows for the 9 business hours 9-5pm
-
-
- var myTimes = ["9AM","10AM","11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-    var myTimesLength = myTimes.length;
-    for (i = 0; i < myTimesLength; i++) {
-    // create the rows within which we will create the time blocks
-    var newRow = $('<div>').addClass ('row hour').text(myTimes[i]);
-    // color coordinate the times
-
-     
-
-    if 
-    
-    
-    
-    
-
-
-    //append the rows within the container
-    $('.container').append(newRow);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // create the timeblocks that contain the content
-   // var timeBlock = $('<div>').addClass('time-block textarea')
-
-   // var textArea = $('#myInput').addClass('textarea').html('');
-   // var writingBlock = timeBlock.append(textArea);
-
-  
-   // $(newRow).append(writingBlock);
-
-    //console.log("hsdfg");
-
-
-    //var txt = $("#txtArea").val();
-    
-
-    // do an if statement for the past, present future classes
-
-}
-
-
-//$( "input[type=checkbox][name=bar]:checked" ).val();
-
-
-
-
-
-
-
-// For loop to print rows with timeblocks, taskblocks, and save buttons
-//for (i = 0; i < 9; i++) {
+for (i = 0; i < 9; i++) {
     //create a variable for the row
-    //var rowBlock = $('<div>').addClass('row');
+    var rowBlock = $('<div>').addClass('row');
     //create a variable for the time block
-   // var timeBlock = $('<div>').addClass('hour col-md-2').text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
-    //timeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
+    var timeBlock = $('<div>').addClass('hour col-md-2').text(moment('9:00 AM', 'hh:mm A').add(i,'hours').format('hA'));
 
     //create a variable for the taskblock
-   // var taskBlock = $('<textarea>').addClass('col-md-9');
+    var taskBlock = $('<textarea>').addClass('col-md-9');
     //create a variable for the save block
-   // var saveButton = $('<button>').addClass('saveBtn col-md-1').html('<i class="fas fa-save"></i>');
+    var saveButton = $('<button>').addClass('saveBtn col-md-1').html('<i class="fas fa-save"></i>');
 
     // Placing content created above into the DOM in the right order.
     //append the container with the row
-   // $('.container').append(rowBlock);
+    $('.container').append(rowBlock);
     //append the row with the time block
-   // $(rowBlock).append(timeBlock);
-    //after the timeblock display the task block
-//$(timeBlock).after(taskBlock);
-   // //after the taskblock display the save button
-  //  $(taskBlock).after(saveButton);
-//}
+    $(rowBlock).append(timeBlock).append(taskBlock).append(saveButton);
+
+
+if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'))) {
+    $(taskBlock).addClass('present');
+    //if the time is in the future, display green
+    } else if (now.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'))) {
+    $(taskBlock).addClass('future');
+    //if the time is in the past, display grey
+    } else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'))) {
+    $(taskBlock).addClass('past');
+    }
+}
+
+
