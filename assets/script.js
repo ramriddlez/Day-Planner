f0a91b9f80cdf830471d6d79220074bb407c30cf
ramriddlez <ramankangz@outlook.com>
2022-02-02 16:23:00 -0500

    changes in logic

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,34 @@
 //use moment.js to format the following variables:
 let today = moment().format("dddd, MMMM Do YYYY,");
-let now = moment();
+let now = moment().hour(Number);
 
 
 //this will be the time block using JQuery
 $('#currentDay').text(today);
+console.log(now)
 
-
-for (i = 0; i< 9; i++) {
+for (let i = 9; i< 17; i++) {
    
-    let i = ['9AM', '10AM','11AM','12PM','1PM','2PM','3PM','4PM'];
+    let array = ['9','10','11','12','13','14','15','16','17'];
     
-    if (now === moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
-        $('col-8').addClass('present') 
-    } else if 
-        (now > moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
-        $('col-8').addClass('past') 
+//get the actual value from now(hour)
+//get the element by their ids and get the hour value from the div
 
-    } else 
-        (now < moment('9:00 AM', 'hh:mm A').add(i, 'hours'))
-        $('col-8').addClass('future') 
+//let id=docuemnt.querySelector(`#${i}`).id
+//<div>
+
+//now =3
+//id=9
+//now===id
+  
+    // if (now === moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
+    //     $('col-8').addClass('present') 
+    // } else if 
+    //     (now > moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
+    //     $('col-8').addClass('past') 
+
+    // } else 
+    //     (now < moment('9:00 AM', 'hh:mm A').add(i, 'hours'))
+    //     $('col-8').addClass('future') 
 }
 
-console.log ('ok');
