8786efc5a83ce8faf360fd1df27f6b628e88e97b
ramriddlez <ramankangz@outlook.com>
2022-02-02 18:06:17 -0500

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,37 @@
+
 //use moment.js to format the following variables:
 let today = moment().format("dddd, MMMM Do YYYY,");
-let now = moment().hour(Number);
-
-
+let now = moment().format('k');
 //this will be the time block using JQuery
 $('#currentDay').text(today);
-console.log(now)
 
-for (let i = 9; i< 17; i++) {
+// get variable for the div
+
+var divE1 = document.querySelector('col-8')
+
+console.log(divE1);
+
+//for (let i = 9; i< 18; i++) {
+
+   //let array = ['9','10','11','12','13','14','15','16','17',];
    
-    let array = ['9','10','11','12','13','14','15','16','17'];
-    
+  // console.log(i);
 //get the actual value from now(hour)
 //get the element by their ids and get the hour value from the div
-
-//let id=docuemnt.querySelector(`#${i}`).id
-//<div>
-
-//now =3
-//id=9
-//now===id
+ //let id = document.querySelector()
+     
+    //now =3
+    //id=9
+    //now===id
   
-    // if (now === moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
-    //     $('col-8').addClass('present') 
-    // } else if 
-    //     (now > moment('9:00 AM', 'hh:mm A').add(i, 'hours')) {
-    //     $('col-8').addClass('past') 
+//      if (now === (i)) {
+//         divE1.setAttribute("style", "background-color: #82E0AA;" ) 
+//      } else if 
+//          (now > i) {
+//         divE1.setAttribute("style", "background-color: #82E0AA;" ) 
+//      } else 
+//        (now < i)
+//        divE1.setAttribute("style", "background-color: #82E0AA;" ) 
+//     }
 
-    // } else 
-    //     (now < moment('9:00 AM', 'hh:mm A').add(i, 'hours'))
-    //     $('col-8').addClass('future') 
-}
-
+// console.log ('ok');
