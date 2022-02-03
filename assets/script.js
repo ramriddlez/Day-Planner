a146e7d63d4c2d89bdb36cd4b1590e846b06333f
ramriddlez <ramankangz@outlook.com>
2022-02-02 19:27:02 -0500

    SUCCESS in javascript time code

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,30 @@
 
 //use moment.js to format the following variables:
 let today = moment().format("dddd, MMMM Do YYYY,");
-let now = moment().format('k');
+let timeNow = moment().format('k');
 //this will be the time block using JQuery
 $('#currentDay').text(today);
 
-// get variable for the div
 
-var userClicked = $(this).parent().attr('id');
-
-for (let i = 9; i< 18; i++) {
-    let array = ['9','10','11','12','13','14','15','16','17',];
-   
-  // console.log(i);
-//get the actual value from now(hour)
-//get the element by their ids and get the hour value from the div
- //let id = document.querySelector()
-     
-    //now =3
-    //id=9
-    //now===id
   
-    if (now === i) {
-         userClicked.addClass('present');
-      } else if 
-         (now > i) {
-         userClicked.addClass('past');
-      } else 
-         (now < i) 
-         userClicked.addClass('future');
-};
+    // loop over time blocks
+    $('.col-8').each(function () {
+        
+        var blockTime = parseInt($(this).parent().attr("id"));
+        
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+
+            $(this).addClass('past');
+        }
+        else if (blockTime === timeNow) {
+
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+
+        }
+    })
+
+    
