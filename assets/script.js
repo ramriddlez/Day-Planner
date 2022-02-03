826ef02c45638bca8107aee6c63099bb14df509d
ramriddlez <ramankangz@outlook.com>
2022-02-02 22:42:12 -0500

    changes in LS

--- assets/script.js
+++ assets/script.js
@@ -35,11 +35,18 @@ $(".saveBtn").on("click", function () {
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
 })
-
-
-    localStorage.getItem("time");
-    localStorage.getItem("text");
+//retrieve local storage
+//
+$("#9AM .description").val(localStorage.getItem("9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
    
-}
+
 
